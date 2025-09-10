3a60a5018de3d859df29a0b1f97dae5c6d677a3d
andrej bartko <andrej@Mac.fritz.box>
2025-09-10 21:33:56 +0100

    update

--- demo/stub/deploy.sh
+++ demo/stub/deploy.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+
+STACK=ice-sim
+REGION=eu-west-1                # or your region
+PROFILE=dev                     # omit if using default profile
+
+# From your hub VPN config download (the VGW config):
+TUN1_OUTSIDE_IP=52.211.164.164   # <-- replace with "Outside IP Address" for Tunnel 1
+TUN2_OUTSIDE_IP=54.217.2.73   # <-- replace with "Outside IP Address" for Tunnel 2
+
+# Optional: prefixes/port (defaults shown)
+ICE_PUBLIC_PREFIX=203.0.113.0/24
+BACKEND_PRIVATE_PREFIX=10.200.1.0/24
+BACKEND_PORT=8080
+
+# ipsec conf tunnels
+LEFT_SUBNET=10.100.2.0/27
+RIGHT_SUBNET=203.0.113.0/24
+
+# Enter PSKs securely (won't be stored in shell history)
+# read -s -p "Enter PSK for Tunnel 1: " PSK1; echo
+# read -s -p "Enter PSK for Tunnel 2: " PSK2; echo
+#
+# simulated only Mahwah
+PSK1=jpLjsf7DeDTMVagrCjXOjCX7LiL7baG5dgYREVbafjNOupyY
+PSK2=k3NSuR5Iskoj5MElebh9WdsQdZH2j6JVX3j75rZVAedkE1Tl
+
+
 echo "Starting deployment script..."
 set -e
 # print all arguments passed to the script as environment variables
