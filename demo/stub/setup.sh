d9e2a2cd3044017fae3736481ef9cd939e1b2df3
andrej bartko <andrej@Mac.fritz.box>
2025-09-10 11:07:15 +0100

    update

--- demo/stub/setup.sh
+++ demo/stub/setup.sh
@@ -7,13 +7,17 @@ REGION=eu-west-1                # or your region
 PROFILE=dev                     # omit if using default profile
 
 # From your hub VPN config download (the VGW config):
-TUN1_OUTSIDE_IP=34.255.45.111   # <-- replace with "Outside IP Address" for Tunnel 1
-TUN2_OUTSIDE_IP=63.34.40.69   # <-- replace with "Outside IP Address" for Tunnel 2
+TUN1_OUTSIDE_IP=52.211.164.164   # <-- replace with "Outside IP Address" for Tunnel 1
+TUN2_OUTSIDE_IP=54.217.2.73   # <-- replace with "Outside IP Address" for Tunnel 2
 
 # Optional: prefixes/port (defaults shown)
-ICE_PUBLIC_PREFIX=158.224.40.0/24
+ICE_PUBLIC_PREFIX=203.0.113.0/24
 BACKEND_PRIVATE_PREFIX=10.200.1.0/24
 BACKEND_PORT=8080
+
+# ipsec conf tunnels
+LEFT_SUBNET=10.100.2.0/27
+RIGHT_SUBNET=203.113.0/24
 
 # Enter PSKs securely (won't be stored in shell history)
 # read -s -p "Enter PSK for Tunnel 1: " PSK1; echo
